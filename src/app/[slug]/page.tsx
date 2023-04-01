e6aff680ea72bdeaa189b556d1e0edcb26560f4e
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2023-04-01 12:59:24 -0300

    refactored innitial files

--- src/app/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-interface ItemsProps {
-   params: {
-      slug: string
-   }
-}
-
-export default function Items({ params }: ItemsProps) {
-   return <h1>Título da página - {params.slug}</h1>
-}
--- src/app/home/style.ts
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import styled from "styled-components"
-
-const Container = styled.div`
-   font-family: system-ui;
-`
-
-export default Container
